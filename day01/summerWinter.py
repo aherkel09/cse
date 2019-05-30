9eeea8f41f6f54f7fccee944997eeecf643478d8
Avery Herkel <aherkel09@gmail.com>
2019-05-30 11:48:14 -0400

    Update summerWinter.py

--- day01/summerWinter.py
+++ day01/summerWinter.py
@@ -9,7 +9,7 @@ def summer_winter(error=False):
     except:
         summer_winter(error=True)
         
-    if num %3 == 0 and num % 5 == 0:
+    if num % 3 == 0 and num % 5 == 0:
         print('SummerWinter')
     elif num % 3 == 0:
         print('Summer')

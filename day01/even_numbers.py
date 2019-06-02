e75784d5fdc1b572bcce79795e74c0c94c7fd88f
Avery Herkel <aherkel09@gmail.com>
2019-06-02 13:20:21 -0400

    Create even_numbers.py

---
+++ day01/even_numbers.py
@@ -0,0 +1,26 @@
+def even_numbers(limit):
+    if limit > 1000:
+        return print('Please choose a number under 1000')
+    count = 0
+
+    while count <= limit:
+        if count % 2 == 0:
+            print(count, 'EVEN')
+        else:
+            print(count, 'ODD')
+        count += 1
+
+    return print('Limit Reached\n')
+
+def get_limit():
+    limit = input('Please enter a number: ')
+
+    try:
+        even_numbers(int(limit))
+        return get_limit()
+    except:
+        print('Error: Please use only integers')
+        return get_limit()
+
+if __name__ == '__main__':
+    get_limit()

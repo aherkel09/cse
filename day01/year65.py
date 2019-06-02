10ff259d58a40644176cf8468db33b28336375f5
Avery Herkel <aherkel09@gmail.com>
2019-06-02 11:55:41 -0400

    simplify

--- day01/year65.py
+++ day01/year65.py
@@ -1,53 +1,19 @@
 import datetime
-import math
 
-def get_name_and_age(error=False):
-    if error:
-        print('\nPlease use only letters for your name & integers for your age.\n')
-        
-    name = check_alpha(input('Please enter your first name: '))
+def get_name_and_age():
+    name = input('Please enter your first name: ')
     age = input('Please enter your age: ')
 
     try:
-        year_65 = get_year_65(float(age))
-        return show_response(name, year_65)
+        get_year_65(float(age))
+        return get_name_and_age()
     except:
-        return get_name_and_age(error=True)
-
-def check_alpha(string):
-    for s in string:
-        if not s.isalpha():
-            return get_name_and_age(error=True)
-    return string
+        return get_name_and_age()
 
 def get_year_65(age):
-    years_remaining = 65 - round(age)
+    year_65 = datetime.date.today().year + (65 - round(age))
     current_year = datetime.date.today().year
-    return current_year + years_remaining
-
-def show_response(name, year):
-    current_year = datetime.date.today().year
-    
-    if year < current_year:
-        tense = 'turned'
-    else:
-        tense = 'will turn'
-
-    message = name + ', you ' + tense + ' 65 in the year ' + str(year) + '.'
-
-    if current_year - (year - 65) > 115 or current_year - (year-65) < 5:
-        message += ' Seems unlikely.'
-
-    print (message)
-    return request_restart()
-
-def request_restart():
-    restart = input('Start again? (y/n): ')
-    if restart.lower() == 'y':
-        return get_name_and_age()
-    else:
-        print('Goodbye.')
-        return
+    print(name + ', you ' + tense + ' 65 in the year ' + str(year) + '.')
 
 if __name__ == '__main__':
     get_name_and_age()

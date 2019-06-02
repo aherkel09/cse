d69bdfe675cb009247815350080711ffd4b32488
Avery Herkel <aherkel09@gmail.com>
2019-06-02 12:55:08 -0400

    simplify

--- day01/driver_speed.py
+++ day01/driver_speed.py
@@ -1,35 +1,22 @@
-import math
-
 def get_speed():
     speed = input('Enter driver speed: ')
 
     try:
-        check_limit(float(speed))
+        get_points(float(speed))
+        return get_speed()
     except:
         print('Please enter an number speed.\n')
-
-    return get_speed()
-
-def check_limit(speed):
-    over_limit = speed - 55
-
-    if over_limit < -55:
-        print('Vehicle is driving backwards.')
-    elif over_limit == -55:
-        print('Vehicle is stopped.')
-    elif over_limit > -55 and over_limit <= -20:
-        print('Vehicle is driving too slowly.')
-    elif over_limit > -20 and over_limit <= 0:
+        return get_speed()
+    
+def get_points(speed):
+    points = (round(speed - 56) // 5) + 1
+    
+    if points < 1:
         print('OK')
+    elif points < 12:
+        print('Points:', str(points))
     else:
-        print(get_points(over_limit))
-
-def get_points(mph_over):
-    points = math.ceil(mph_over/5)
-    if points < 12:
-        return 'Points: ' + str(points)
-    else:
-        return 'License Suspended!'
+        print('License Suspended!')
 
 if __name__ == '__main__':
     get_speed()

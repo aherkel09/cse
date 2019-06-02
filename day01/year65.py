4f3adfd24a86b06c80374a6cef72a974982a6fe8
Avery Herkel <aherkel09@gmail.com>
2019-06-02 12:39:52 -0400

    single function

--- day01/year65.py
+++ day01/year65.py
@@ -1,19 +1,16 @@
 import datetime
 
-def get_name_and_age():
-    name = input('Please enter your first name: ')
+def get_year_65():
+    fname = input('Please enter your first name: ')
     age = input('Please enter your age: ')
 
     try:
-        get_year_65(int(age))
-        return get_name_and_age()
+        year_65 = datetime.date.today().year + (65 - int(age))
+        print(fname + ', you will turn 65 in the year ' + str(year_65) + '.\n')
+        return get_year_65()
     except:
-        print('Error: Please use only letters for your name & numbers for your age')
-        return get_name_and_age()
-
-def get_year_65(age):
-    year_65 = datetime.date.today().year + (65 - age)
-    print(name, ', you will turn 65 in the year ', str(year_65), '.')
+        print('Error: Please use only letters for your name & numbers for your age.\n')
+        return get_year_65()
 
 if __name__ == '__main__':
-    get_name_and_age()
+    get_year_65()

f9659f611716777197018b0d498334536bc9b5bb
Avery Herkel <aherkel09@gmail.com>
2019-06-03 10:54:25 -0400

    import modules, call functions from input

--- day01_master.py
+++ day01_master.py
@@ -1,16 +1,19 @@
-from day01 import year_65, max_numbers, summer_winter
+from day01 import (
+    year_65, max_numbers, summer_winter, driver_speed, even_odd,
+    multiples, check_prime, primes, sort, perfect_number
+)
 
 operations = {
-    1: ('Find out when I will turn 65', year_65.main()),
-    2: ('Get the maximum of 2 numbers', max_numbers.main()),
-    3: ('summer_winter', summer_winter.main()),
-    4: ('Check driver speed', driver_speed.main()),
-    5: ('See the odds & evens below a number', even_odd.main()),
-    6: ('See the multiples of 3 & 5 below a number', multiples.main()),
-    7: ('Check if a number is prime', check_prime.main()),
-    8: ('See all the prime numbers below a number', primes.main()),
-    9: ('See a sorted list of 3 numbers', sort.main()),
-    10: ('Check if a number is a perfect number', perfect_number.main())
+    1: ('Find out when I will turn 65', year_65.get_year_65()),
+    2: ('Get the maximum of 2 numbers', max_numbers.get_max_of_numbers()),
+    3: ('summer_winter', summer_winter.summer_winter()),
+    4: ('Check driver speed', driver_speed.get_speed()),
+    5: ('See the odds & evens below a number', even_odd.get_limit()),
+    6: ('See the multiples of 3 & 5 below a number', multiples.get_number()),
+    # 7: ('Check if a number is prime', check_prime.get_number()),
+    # 8: ('See all the prime numbers below a number', primes.get_limit()),
+    9: ('See a sorted list of 3 numbers', sort.get_numbers()),
+    10: ('Check if a number is a perfect number', perfect_number.get_number())
 }
 
 def get_operation():

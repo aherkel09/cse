61957be33cb4780808ef43fd516a1e0a2e3c2d7a
Avery Herkel <aherkel09@gmail.com>
2019-06-03 16:01:11 -0400

    Update primes.py

--- day01/primes.py
+++ day01/primes.py
@@ -1,1 +1,24 @@
+import check_prime as prime
 
+def get_primes(limit):
+    primes = [2]
+    count = 2
+
+    while count < limit:
+        if count not in primes and prime.check_prime(count):
+            primes += [count]
+        count += 1
+    
+    return print(primes)
+
+def get_number():
+    num = input('Please enter a number: ')
+    
+    try:
+        get_primes(int(num))
+    except:
+        print('Please enter only positive integers\n')
+        get_number()
+
+if __name__ == '__main__':
+    get_number()

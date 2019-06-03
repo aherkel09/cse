3a2e8deccc901fd2bf2781cf79298359c900450b
Avery Herkel <aherkel09@gmail.com>
2019-06-03 13:47:51 -0400

    Create day01_functions.py

---
+++ day01_functions.py
@@ -0,0 +1,22 @@
+def set_functions():
+    from day01 import (
+        year_65, max_numbers, summer_winter, driver_speed, even_odd,
+        multiples, #check_prime, primes, <-- pending completion
+        sort, perfect_number
+    )
+
+    functions = {
+        1: ('Find out when you will turn 65', year_65.get_year_65),
+        2: ('Get the maximum of 2 numbers', max_numbers.get_max_of_numbers),
+        3: ('Summer/Winter', summer_winter.summer_winter),
+        4: ('Check driver speed', driver_speed.get_speed),
+        5: ('See the odds & evens below a number', even_odd.get_limit),
+        6: ('See the multiples of 3 & 5 below a number', multiples.get_number),
+        # 7: ('Check if a number is prime', check_prime.get_number), <-- pending completion
+        # 8: ('See all the prime numbers below a number', primes.get_limit), <-- pending completion
+        9: ('See a sorted list of 3 numbers', sort.get_numbers),
+        10: ('Check if a number is a perfect number', perfect_number.get_number),
+        11: ('Exit', 'exit')
+    }
+    
+    return functions

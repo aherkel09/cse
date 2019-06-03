160cc9b47c290ddc31d3ad6596c4ae02ae19cd38
Avery Herkel <aherkel09@gmail.com>
2019-06-03 14:44:12 -0400

    Update check_prime.py

--- day01/check_prime.py
+++ day01/check_prime.py
@@ -1,1 +1,25 @@
+def check_prime(num):
+    if num < 2:
+        return false
+    elif num == 2:
+        return True
+    
+    count = 2
+    while count < num:
+        if count % num == 0:
+            return False
+        count += 1
+    
+    return True
 
+def get_number():
+    num = input('Please enter a number: ')
+    
+    try:
+        print(check_prime(int(num)))
+    except:
+        print('Please enter only positive integers\n')
+        get_number()
+
+if __name__ == '__main__':
+    get_number()

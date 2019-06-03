42c1244e15f1e251a072385b4a9a4c7376c41738
Avery Herkel <aherkel09@gmail.com>
2019-06-03 10:02:15 -0400

    Create perfect_number.py

---
+++ day01/perfect_number.py
@@ -0,0 +1,29 @@
+def is_perfect_number(num):
+    if num < 0:
+        return print('\n') # no result if num < 0
+        
+    count = 1
+    sum_divisors = 0
+    
+    while count < num:
+        if num % count == 0:
+            sum_divisors += count
+        count += 1
+    
+    if sum_divisors == num:
+        return print(num, 'is a perfect number\n')
+    else:
+        return print(num, 'is not a perfect number\n')
+
+def get_number():
+    num = input('Please enter a positive integer: ')
+
+    try:
+        is_perfect_number(int(num)) # convert to int & check number
+        return get_number()
+    except:
+        print('Please enter only positive integers\n')
+        return get_number() # request new input on error
+
+if __name__ == '__main__':
+    get_number()

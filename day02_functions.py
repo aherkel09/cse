50c122848958b4825e0795540da532f295fbfc0d
Avery Herkel <aherkel09@gmail.com>
2019-06-04 10:43:32 -0400

    add reverse_words

--- day02_functions.py
+++ day02_functions.py
@@ -1,11 +1,12 @@
 def set_functions():
     from day02 import (
-        palindrome, fibonacci
+        palindrome, fibonacci, reverse_words
     )
 
     functions = {
         1: ('See if a string is a palindrome', palindrome.get_string),
         2: ('List the Fibonacci numbers up to a limit', fibonacci.get_number),
+        3: ('Print a string of words in reverse', reverse_words.get_string),
         11: ('Exit', 'exit'),
     }
 

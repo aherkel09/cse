7355c9db96d661d136af14619b6744f2f9f489cd
Avery Herkel <aherkel09@gmail.com>
2019-06-04 10:31:50 -0400

    Update reverse_words.py

--- day02/reverse_words.py
+++ day02/reverse_words.py
@@ -1,1 +1,21 @@
+def reverse_words(string):
+    words = string.split(' ')
+    reverse = ''
+    
+    for i in range(1, len(words)+1):
+        reverse += words[0-i] + ' '
+    
+    return reverse
 
+def get_string():
+    string = input('Please enter a string of words: ')
+    
+    try:
+        print(reverse_words(string))
+    except Exception as error:
+        print('Error:', error)
+        get_string()
+        
+if __name__ == '__main__':
+    get_string()
+    

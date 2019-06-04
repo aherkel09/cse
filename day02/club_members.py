2b0e0b76f4450947b0e6ec0c753654fa210de542
Avery Herkel <aherkel09@gmail.com>
2019-06-04 13:22:33 -0400

    Update club_members.py

--- day02/club_members.py
+++ day02/club_members.py
@@ -1,1 +1,11 @@
+def add_member(members):
+  member = input("Please enter a member's name (or type 'stop' to exit): ")
 
+  if member != 'stop':
+    members += [member]
+    return add_member(members)
+  else:
+    print(members)
+
+if __name__ == '__main__':
+  add_member([])

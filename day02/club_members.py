91a026b31686256c4884cd84a2828da6744d1438
Avery Herkel <aherkel09@gmail.com>
2019-06-05 10:21:25 -0400

    change prompt & spacing

--- day02/club_members.py
+++ day02/club_members.py
@@ -1,11 +1,11 @@
 def add_member(members=[]):
-  member = input("Please enter a member's name (or type 'stop' to exit): ")
+    member = input("Please enter a member's name (or type 'done' to exit): ")
 
-  if member != 'stop':
-    members += [member]
-    return add_member(members)
-  else:
-    print(members)
+    if member != 'done':
+        members += [member]
+        return add_member(members)
+    else:
+        print(members)
 
 if __name__ == '__main__':
-  add_member()
+    add_member()

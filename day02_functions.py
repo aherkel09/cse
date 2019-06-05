fa2d01543f95080195d4325750f9821c87ee2225
Avery Herkel <aherkel09@gmail.com>
2019-06-05 12:00:59 -0400

    add grades

--- day02_functions.py
+++ day02_functions.py
@@ -1,6 +1,7 @@
 def set_functions():
     from day02 import (
-        palindrome, fibonacci, reverse_words, club_members, password
+        palindrome, fibonacci, reverse_words, club_members, grades,
+        password
     )
 
     functions = {
@@ -8,6 +9,7 @@ def set_functions():
         2: ('List the Fibonacci numbers up to a limit', fibonacci.get_number),
         3: ('Print a string of words in reverse', reverse_words.get_string),
         4: ('Create a list of club members', club_members.add_member),
+        5: ('See total & average grades', grades.add_grade)
         6: ('Check if your password is valid', password.get_password),
         11: ('Exit', 'exit'),
     }

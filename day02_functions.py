9286f56e4156232c775ee78c9435b46bef504e59
Avery Herkel <aherkel09@gmail.com>
2019-06-05 12:04:51 -0400

    dict syntax

--- day02_functions.py
+++ day02_functions.py
@@ -9,7 +9,7 @@ def set_functions():
         2: ('List the Fibonacci numbers up to a limit', fibonacci.get_number),
         3: ('Print a string of words in reverse', reverse_words.get_string),
         4: ('Create a list of club members', club_members.add_member),
-        5: ('See total & average grades', grades.add_grade)
+        5: ('See total & average grades', grades.add_grade),
         6: ('Check if your password is valid', password.get_password),
         11: ('Exit', 'exit'),
     }

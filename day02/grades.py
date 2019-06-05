eef2a2377fb7b70504ca7ae82f0ab0888a4d23ba
Avery Herkel <aherkel09@gmail.com>
2019-06-05 10:41:05 -0400

    Update grades.py

--- day02/grades.py
+++ day02/grades.py
@@ -1,1 +1,17 @@
+def add_grade(grades=[], total=0):
+    new_grade = input('Please enter a test grade (type "done" to exit): ')
 
+    if new_grade == 'done':
+        print('Grades:', grades, '\nTotal:', total, '\nAverage', total/len(grades))
+    else:
+        try:
+            new_grade = abs(float(new_grade)) # try converting to float, use abs to ensure new_grade is positive
+            grades += [new_grade]
+            total += new_grade
+        except:
+            print('Error: please enter only numbers\n')
+        finally:
+            return add_grade(grades, total) # always ask user for another grade       
+
+if __name__ == '__main__':
+    add_grade()

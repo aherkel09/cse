af37cefbcf318f77cc92df493c89184c247f7e0f
Avery Herkel <aherkel09@gmail.com>
2019-06-05 23:08:19 -0400

    make sure all inputs are numbers

--- day03/arithmeticMean.js
+++ day03/arithmeticMean.js
@@ -1,1 +1,32 @@
+function arithmeticMean(numbers) {
+  var total = 0;
+  
+  for (var n in numbers) {
+    total += Number(numbers[n]);
+  }
+  
+  if (numbers.length) {
+    console.log(total + ' / ' + numbers.length);
+    var mean = total / numbers.length
+    return 'The arithmetic mean of ' + String(numbers) + ' is ' + mean;
+  }
+  
+  return 'No numbers were entered';
+}
 
+function getNumbers() {
+  var counter = 0;
+  var numbers_list = prompt('Enter a list of numbers separated by a space: ').split(' ');
+  
+  for (var l in numbers_list) {
+    counter += isNaN(numbers_list[l]);
+  }
+  
+  if (counter > 0) {
+    alert('Error: please enter only numbers');
+  } else if (counter == 0) {
+    alert(arithmeticMean(numbers_list));
+  }
+}
+  
+  

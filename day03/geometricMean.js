f8d904f65764a6489e7ed8fd65e4d575fadf34d2
Avery Herkel <aherkel09@gmail.com>
2019-06-06 10:25:05 -0400

    Update geometricMean.js

--- day03/geometricMean.js
+++ day03/geometricMean.js
@@ -1,1 +1,29 @@
+function geometricMean(numbers) {
+  var product = 1;
+  
+  for (var n in numbers) {
+    product *= numbers[n];
+  }
+  
+  if (numbers.length) {
+    var mean = product ** (1/numbers.length);
+    return 'The geometric mean of ' + numbers + ' is ' + mean;
+  } else {
+    return 'No numbers were entered';
+  }
+}
 
+function getNumbers() {
+  var counter = 0;
+  var numbers_list = prompt('Enter a list of numbers separated by a space: ').split(' ');
+  
+  for (var l in numbers_list) {
+    counter += isNaN(numbers_list[l]);
+  }
+  
+  if (counter > 0) {
+    alert('Error: please enter only numbers');
+  } else if (counter == 0) {
+    alert(geometricMean(numbers_list));
+  }
+}

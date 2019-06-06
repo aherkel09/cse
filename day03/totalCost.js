3c32535554b2b68c6ef5bbb4504f31dd3d5db2f8
Avery Herkel <aherkel09@gmail.com>
2019-06-06 12:39:21 -0400

    error handling

--- day03/totalCost.js
+++ day03/totalCost.js
@@ -3,7 +3,12 @@ function totalCost(price, num){
     alert(totalCost);
 }
 function getTotal(){
- var price = prompt("Please enter price of item.");
- var number = prompt("Please enter number of items.");
-  totalCost(price, number);
+  var price = prompt("Please enter price of item.").split('$')[0];
+  var number = prompt("Please enter number of items.");
+  
+  if (!isNaN(price) && !isNaN(number)) {
+    totalCost(price, number);
+  } else {
+    alert('Error: please enter only numbers.');
+    getTotal();
 }

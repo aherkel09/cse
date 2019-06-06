02c41f2e8593b7c3f183d68e299559d9c8c72772
Avery Herkel <aherkel09@gmail.com>
2019-06-06 15:57:19 -0400

    error handling

--- day03/invoice.js
+++ day03/invoice.js
@@ -1,1 +1,20 @@
+function computeCost(price, numItems) {
+  var pretax = price * numItems;
+  var shipping = pretax * 0.1;
+  var tax = pretax * 0.08;
+  
+  alert(pretax + shipping + tax);
+}
 
+function getOrderDetails() {
+  var price = prompt('Enter the price of the item:').replace('$', '');
+  var num = prompt('Enter the number of items:');
+  
+  if (!isNaN(price) && !isNaN(num)) {
+    return computeCost(price, num);
+  } else {
+    alert('Error: please enter only numbers');
+    getOrderDetails();
+  }
+}
+  

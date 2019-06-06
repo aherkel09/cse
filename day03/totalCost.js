0bd799cb4592ce68bc0af251a284d3a26db748b2
narmeend13 <51205536+narmeend13@users.noreply.github.com>
2019-06-06 12:28:30 -0400

    Update totalCost.js

--- day03/totalCost.js
+++ day03/totalCost.js
@@ -1,1 +1,9 @@
 function totalCost(price, num){
+  var totalCost = price*num;
+    alert(totalCost);
+}
+function getTotal(){
+ var price = prompt("Please enter price of item.");
+ var number = prompt("Please enter number of items.");
+  totalCost(price, number);
+}

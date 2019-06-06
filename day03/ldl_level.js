363edd50afb53ad60311e872f12c27ca72d2ad1a
Avery Herkel <aherkel09@gmail.com>
2019-06-05 20:57:38 -0400

    Update ldl_level.js

--- day03/ldl_level.js
+++ day03/ldl_level.js
@@ -1,1 +1,29 @@
+function ldl_level(level) {
+  var level_range = {
+    100: 'optimal',
+    130: 'near optimal',
+    160: 'borderline',
+    190: 'high',
+  }
+  
+  for (r in level_range) {
+    if (level < r) {
+      return level_range[r];
+    }
+  }
+  
+  return 'very high';
+}
 
+function get_level() {
+  var ldl = prompt('Please enter your LDL level: ')
+  
+  try {
+    var level = ldl_level(Math.abs(Number(ldl)));
+    console.log('Your LDL level is', level);
+  } catch {
+    console.log('Error: please enter only numbers');
+  }
+}
+
+get_level();

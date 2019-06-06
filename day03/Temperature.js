18bb728256c8e92b5f5a06f40f38be028be6e8fa
Avery Herkel <aherkel09@gmail.com>
2019-06-06 12:53:48 -0400

    input handling

--- day03/Temperature.js
+++ day03/Temperature.js
@@ -1,13 +1,18 @@
 function weather(temperature){
   if (temperature < 65){
     alert("cold");
-  }else if (temperature > 85){
+  } else if (temperature > 85){
     alert("hot");
-  }else{
+  } else {
     alert("comfortable");
   }
 }
 function getTemp(){
   var temp = prompt("Please enter a temperature");
-  return weather(temp);
+  if (!isNaN(temp)) { // make sure user input number
+    return weather(temp);
+  } else {
+    alert('Error: please enter only numbers');
+    return getTemp();
+  }
 }

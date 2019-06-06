69d976887f15fa2fde715c10e40c01b3266155fc
narmeend13 <51205536+narmeend13@users.noreply.github.com>
2019-06-06 12:04:24 -0400

    Update Temperature.js

--- day03/Temperature.js
+++ day03/Temperature.js
@@ -1,10 +1,9 @@
 function weather(temperature){
   if (temperature < 65){
     alert("cold");
-  }else{
+  }else if (temperature > 85){
     alert("hot");
+  }else{
+    alert("comfortable");
   }
 }
-    
-    
-weather(temperature)

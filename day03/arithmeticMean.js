6f1d52747866d22e12a2802f20faacece0545d11
Avery Herkel <aherkel09@gmail.com>
2019-06-06 16:56:56 -0400

    simplify error handling

--- day03/arithmeticMean.js
+++ day03/arithmeticMean.js
@@ -5,12 +5,8 @@ function arithmeticMean(numbers) {
     total += Number(numbers[n]);
   }
   
-  if (numbers.length) {
-    var mean = total / numbers.length
-    return 'The arithmetic mean of ' + numbers + ' is ' + mean;
-  }
-  
-  return 'No numbers were entered';
+  var mean = total / numbers.length;
+  return 'The arithmetic mean of ' + numbers + ' is ' + mean;
 }
 
 function getArithNumbers() {
@@ -18,13 +14,15 @@ function getArithNumbers() {
   var numbers_list = prompt('Enter a list of numbers separated by a space: ').split(' ');
   
   for (var l in numbers_list) {
-    counter += isNaN(numbers_list[l]);
+    counter += isNaN(numbers_list[l]); // adds 1 to counter if non-numeric character in numbers_list
   }
   
   if (counter > 0) {
     alert('Error: please enter only numbers');
-  } else if (counter == 0) {
+  } else if (counter == 0 && numbers_list.length) { // check length to avoid dividing by 0
     alert(arithmeticMean(numbers_list));
+  } else {
+    alert('No numbers were entered');
   }
 }
   

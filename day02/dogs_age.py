1611c5bab7642569600f56376cc89f2a8111aa47
Avery Herkel <aherkel09@gmail.com>
2019-06-11 11:53:01 -0400

    error handling

--- day02/dogs_age.py
+++ day02/dogs_age.py
@@ -1,10 +1,19 @@
-def dogs_age():
-  dogs_age = int(input("Input a dog's age in human years: "))
-  if dogs_age <= 2:
-	  human_age = 10.5
-  else:
-	  human_age = 21 + (dogs_age - 2) * 4
+def get_dogs_age(years):
+	if years <= 2:
+		human_age = 10.5 * years
+	else:
+		human_age = 21 + (years - 2) * 4
 
-  print("The dog's age in dog's years is: ", human_age)      
+	print("The dog's age in dog's years is", human_age)      
 
-dogs_age()                 
+def get_age():              
+	years = input("Input a dog's age in human years: ")
+	
+	try:
+		get_dogs_age(int(years))
+	except:
+		print('Error: please enter only numbers')
+		get_age()
+
+if __name__ == '__main__':
+	get_age()

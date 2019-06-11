277b339fabfffd92a1677e4402e8a3f582bb252a
narmeend13 <51205536+narmeend13@users.noreply.github.com>
2019-06-11 00:27:16 -0400

    Update dogs_age.py

--- day02/dogs_age.py
+++ day02/dogs_age.py
@@ -1,1 +1,10 @@
+def dogs_age():
+  dogs_age = int(input("Input a dog's age in human years: "))
+  if dogs_age <= 2:
+	  human_age = 10.5
+  else:
+	  human_age = 21 + (dogs_age - 2) * 4
 
+  print("The dog's age in dog's years is: ", human_age)      
+
+dogs_age()                 

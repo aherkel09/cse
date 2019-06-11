21d80e1bfc882f46c76811df360375ba059cf328
narmeend13 <51205536+narmeend13@users.noreply.github.com>
2019-06-10 23:22:42 -0400

    Update integer.py

--- day02/integer.py
+++ day02/integer.py
@@ -1,11 +1,11 @@
 def integer():
-  n=int(input("enter an integer greater than 1: "))
-  sum=0
-  for i in range(1,n+1):
-    sum=sum+i
+  n = int(input("enter an integer greater than 1: "))
+  sum = 0
+  for i in range(1,n + 1):
+    sum = sum+i
   
-  bs = sum/n
-  print(bs)
+  avg = sum/n
+  print(avg)
 
 integer()
 

cef0529e61948a49ad1099dcca58b708334a6977
narmeend13 <51205536+narmeend13@users.noreply.github.com>
2019-06-10 23:19:01 -0400

    Update integer.py

--- day02/integer.py
+++ day02/integer.py
@@ -1,2 +1,15 @@
-def check(integer):
+def integer():
+  n=int(input("enter an integer greater than 1: "))
+  sum=0
+  for i in range(1,n+1):
+    sum=sum+i
+  
+  bs = sum/n
+  print(bs)
+
+integer()
+
+
+  
+  
   

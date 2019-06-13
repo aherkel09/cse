41ae3be63f22827c7ea790d13d28c78d17833e0f
Avery Herkel <aherkel09@gmail.com>
2019-06-13 10:14:57 -0400

    reorder, rename

--- day02/even_digits.py
+++ day02/even_digits.py
@@ -1,23 +1,22 @@
-def get_limit():
-    limit = input("enter a number: ")
-    
-    try:
-        limit = int(limit)
-        for i in range(1, limit + 1):
-            if isEven(i) == True:
-                print(i)
-    except:
-        print('Error: please enter only numbers')
-        get_limit()
-
-
-def isEven(num):
+def is_even(num):
     while num > 0:
         digit = num % 10 # get last digit
         if digit % 2 != 0:
             return False
         num = num // 10 # remove last digit & start over
     return True
+
+def get_limit():
+    limit = input("enter a number: ")
+    
+    try:
+        limit = int(limit)
+        for i in range(1, limit + 1):
+            if is_even(i) == True:
+                print(i)
+    except:
+        print('Error: please enter only numbers')
+        get_limit()
 
 if __name__ == '__main__':
     get_limit()

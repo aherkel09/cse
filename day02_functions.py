a265ffd0e92819002529b1ca82f8a5353994a4d5
Avery Herkel <aherkel09@gmail.com>
2019-06-13 10:33:40 -0400

    add 7 - 10

--- day02_functions.py
+++ day02_functions.py
@@ -1,7 +1,7 @@
 def set_functions():
     from day02 import (
         palindrome, fibonacci, reverse_words, club_members, grades,
-        password
+        password, average, even_digits, dogs_age, next_date
     )
 
     functions = {
@@ -11,6 +11,10 @@ def set_functions():
         4: ('Create a list of club members', club_members.add_member),
         5: ('See total & average grades', grades.add_grade),
         6: ('Check if your password is valid', password.get_password),
+        7: ('Calculate the average of a set of numbers up to a limit', average.get_number),
+        8: ('See all the numbers with only even digits up to a limit', even_digits.get_limit),
+        9: ('Convert a dog\'s age to human years', dogs_age.get_age),
+        10: ('Enter a date to see the date on the following day', next_date.get_start_date)
     }
 
     return functions

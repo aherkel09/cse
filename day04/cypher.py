5a3c19c0d5c260e2e0e904de49c96898bcd12e35
Avery Herkel <aherkel09@gmail.com>
2019-06-13 13:35:12 -0400

    capture capitals

--- day04/cypher.py
+++ day04/cypher.py
@@ -1,14 +1,19 @@
 def advance_three_letters(string):
   alpha = 'abcdefghijklmnopqrstuvwxyz'
+  caps = alpha.upper()
   advanced = ''
 
   for s in string:
-    if s in alpha:
+    if s in alpha or s in caps:
       for a in range(len(alpha)):
         if alpha[a] == s and a <= 22:
-          advanced += alpha[a+3]
+          advanced += alpha[a + 3]
         elif alpha[a] == s:
           advanced += alpha[a - 23]
+        elif caps[a] == s and a <= 22:
+          advanced += caps[a + 3]
+        elif caps[a] == s:
+          advanced += caps[a - 23]
     else:
       advanced += s
 

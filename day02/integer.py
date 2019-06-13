5851ae457e76cf7c82de84a7de384fb5feb2cc52
Avery Herkel <aherkel09@gmail.com>
2019-06-13 10:13:23 -0400

    error handling

--- day02/integer.py
+++ day02/integer.py
@@ -1,15 +1,20 @@
-def integer():
-  n = int(input("enter an integer greater than 1: "))
-  sum = 0
-  for i in range(1, n + 1):
-    sum = sum + i
-  
-  avg = sum / n
-  print(avg)
+def get_average(limit):
+    sum = 0
+    for i in range(1, limit + 1):
+        sum += i
 
-integer()
+    avg = sum / limit
+    return avg
 
+def get_number():
+    number = input("Please enter an integer greater than 1: ")
 
-  
-  
-  
+    try:
+        print(get_average(abs(int(number))))
+    except:
+        print('Error: please enter only positive integers greater than 1')
+        get_number()
+
+if __name__ == '__main__':
+    get_number()
+    

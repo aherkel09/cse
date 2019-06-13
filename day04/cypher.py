c5c8541c865fc2f9f2f6e54a5f3cebe1051ca78c
Avery Herkel <aherkel09@gmail.com>
2019-06-13 11:54:51 -0400

    Update cypher.py

--- day04/cypher.py
+++ day04/cypher.py
@@ -1,1 +1,27 @@
+def advance_three_letters(string):
+  alpha = 'abcdefghijklmnopqrstuvwxyz'
+  advanced = ''
 
+  for s in string:
+    if s in alpha:
+      for a in range(len(alpha)):
+        if alpha[a] == s and a <= 22:
+          advanced += alpha[a+3]
+        elif alpha[a] == s:
+          advanced += alpha[a - 23]
+    else:
+      advanced += s
+
+  print(advanced)
+
+def get_string():
+  string = input('Please enter a string of letters & words: ')
+
+  try:
+    advance_three_letters(string)
+  except Exception as error:
+    print('Error:', error)
+    get_string()
+
+if __name__ == '__main__':
+  get_string()

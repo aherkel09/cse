519e71733617554f838fe0f2e996b586d8627043
Avery Herkel <aherkel09@gmail.com>
2019-06-13 10:02:49 -0400

    simplify, alter function name

--- day02/password.py
+++ day02/password.py
@@ -1,29 +1,4 @@
-def checkval(password):
-    has_length = False
-    has_lc = False
-    has_uc = False
-    has_num = False
-    has_special = False
-    
-    if len(password) >= 6 and len(password) <= 16:
-        has_length = True
-    
-        for p in password:
-            if p.isdigit():
-                has_num = True
-            elif p.islower():
-                has_lc = True
-            elif p.isupper():
-                has_uc = True
-            elif p in ['$', '#', "@"]:
-                has_special = True
-                
-    if has_length and has_lc and has_uc and has_num and has_special:
-        return(print("Password is valid."))
-    
-    return get_password(invalid=True)
-
-def check_val_2(password):
+def check_valid(password):
     valid = [False, False, False, False, False]
     
     if len(password) >= 6 and len(password) <= 16:
@@ -55,7 +30,7 @@ def get_password(invalid=False):
     password = input('Please enter a password: ')
     
     try:
-        checkval(password)
+        check_valid(password)
     except Exception as error:
         print('Error:', error)
         get_password()

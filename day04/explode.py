a6ea24558d15fd2545fe30d630ed1d1fefee41bb
Avery Herkel <aherkel09@gmail.com>
2019-06-14 09:23:22 -0400

    Update explode.py

--- day04/explode.py
+++ day04/explode.py
@@ -1,1 +1,18 @@
+def explode(string):
+    char_list = []
+    for s in string:
+        char_list += [s]
+    
+    return char_list
 
+def get_string():
+    string = input('Please enter a string: ')
+    
+    try:
+        print(explode(string))
+    except Exception as error:
+        print('Error:', error)
+        get_string()
+
+if __name__ == '__main__':
+    get_string()

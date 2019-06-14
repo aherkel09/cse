3a88156bf55932e7e861f75d5c307931ef187cb5
Avery Herkel <aherkel09@gmail.com>
2019-06-14 08:30:21 -0400

    splice array, handle errors

--- day04/dropDuplicates.js
+++ day04/dropDuplicates.js
@@ -1,3 +1,20 @@
 function dropDuplicates(arr1, arr2) {
   for (var a in arr1) {
-    if (arr2.includes(arrl // more later...
+    if (arr2.includes(arrl[a])) {
+      arr1.splice(a, 1);
+    }
+  }
+  
+  return arr1;
+}
+
+function getItems() {
+  var first = prompt('Please enter a list of items separated by a comma:');
+  var second = prompt('Please enter another list of items separated by a comma:');
+  
+  try {
+    alert(dropDuplicates(first, second));
+  } catch(error) {
+    console.log(error);
+  }
+}

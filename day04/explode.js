f48970f75a6188b17a39349b315d599ad677daa1
Avery Herkel <aherkel09@gmail.com>
2019-06-14 09:26:48 -0400

    Update explode.js

--- day04/explode.js
+++ day04/explode.js
@@ -1,1 +1,18 @@
+function explode(string) {
+  var charList = [];
+  for (var s in string) {
+    charList.push(string[s]);
+  }
+  
+  return charList;
+}
 
+function getUserString() {
+  var string = prompt('Please enter a string:');
+  
+  try {
+    alert(explode(string));
+  } catch(error) {
+    console.log(error);
+  }
+}

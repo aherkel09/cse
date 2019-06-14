3feb71db52e42b97ee5cb3ddd6f8954a1dff6565
Avery Herkel <aherkel09@gmail.com>
2019-06-14 17:00:25 -0400

    Update random_integers.py

--- day04/random_integers.py
+++ day04/random_integers.py
@@ -1,1 +1,10 @@
+import random
 
+def get_distribution():
+    distribution = [0 for x in range(100)]
+    
+    for x in range(1000000): # 1 million
+        random_int = random.randint(0, 100)
+        distribution[random_int] += 1
+        # more later
+    

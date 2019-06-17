7c6038aaeb1a2d13bed606e3d350f30d87f82516
Avery Herkel <aherkel09@gmail.com>
2019-06-17 09:31:35 -0400

    print distribution as dictionary

--- day04/random_integers.py
+++ day04/random_integers.py
@@ -1,10 +1,16 @@
 import random
 
 def get_distribution():
-    distribution = [0 for x in range(100)]
+    zeros = [x for x in range(101)]
+    distribution = dict.fromkeys(zeros, 0)
     
     for x in range(1000000): # 1 million
         random_int = random.randint(0, 100)
         distribution[random_int] += 1
-        # more later
+    
+    for d in distribution:
+        print(d, distribution[d])
+
+if __name__ == '__main__':
+    get_distribution()
     

d5eaff6ca500e72893e9879d52129364faca5971
Avery Herkel <aherkel09@gmail.com>
2019-06-20 09:01:27 -0400

    Create key_frequency.py

---
+++ day05/key_frequency.py
@@ -0,0 +1,27 @@
+def get_frequency(data, value):
+    freq = 0
+    for d in data:
+        if data[d] == value:
+            freq += 1
+    
+    return str(value) + ' occurs ' + str(freq) + ' time(s).'
+
+def get_data():
+    dictionary = input('Please enter key/value pairs (e.g., key1: value1, key2: value2): ')
+    value = input('Enter a value to see how frequently it occurs: ')
+
+    try:
+        data = {}
+        key_val = dictionary.split(', ')
+
+        for k in range(len(key_val)):
+            split = key_val[k].split(': ')
+            data[split[0]] = split[1]
+
+        print(get_frequency(data, value))
+    except Exception as error:
+        print('Error:', error)
+        get_data()
+    
+if __name__ == '__main__':
+    get_data()

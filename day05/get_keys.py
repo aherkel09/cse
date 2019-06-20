23e460c7736fd6bc198db60e52020f3dff27518f
Avery Herkel <aherkel09@gmail.com>
2019-06-20 09:15:36 -0400

    Create get_keys.py

---
+++ day05/get_keys.py
@@ -0,0 +1,27 @@
+def get_keys_from_value(data, value):
+    keys = []
+    for d in data:
+        if data[d] == value:
+            keys += [d]
+    
+    return str(value) + ' is paired with: ' + str(keys)'
+
+def get_data():
+    dictionary = input('Please enter key/value pairs (e.g., key1: value1, key2: value2): ')
+    value = input('Enter a value to see which keys it is paired with: ')
+
+    try:
+        data = {}
+        key_val = dictionary.split(', ')
+
+        for k in range(len(key_val)):
+            split = key_val[k].split(': ')
+            data[split[0]] = split[1]
+
+        print(get_keys_from_value(data, value))
+    except Exception as error:
+        print('Error:', error)
+        get_data()
+    
+if __name__ == '__main__':
+    get_data()

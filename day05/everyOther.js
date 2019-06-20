ea65ed5159fc4ff83d3ee793b5a1fb2160706070
Avery Herkel <aherkel09@gmail.com>
2019-06-20 09:58:32 -0400

    Update everyOther.js

--- day05/everyOther.js
+++ day05/everyOther.js
@@ -1,5 +1,17 @@
 function everyOther(string) {
+  var everyOther = '';
+  for (var c = 0; c < string.length; c += 2) {
+    everyOther += string[c];
+  }
+  return everyOther;
 }
 
 function getStringToSlice() {
+  var string = prompt('Please enter a string:');
+  
+  try {
+    return alert(everyOther(string));
+  } catch(error) {
+    return console.log(error);
+  }
 }

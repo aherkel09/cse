bc636fe3d173afdf462912077c72a05440a1073a
Avery Herkel <aherkel09@gmail.com>
2019-06-20 10:08:10 -0400

    Update implode.py

--- day05/implode.py
+++ day05/implode.py
@@ -1,1 +1,18 @@
+def implode(string):
+    chars = ''
+    for s in string:
+        if s not in chars:
+            chars += s
+    
+    return chars
 
+def get_string():
+    string = input('Please enter a string: ')
+    
+    try:
+        print(implode(string))
+    except Exception as error:
+        print('Error:', error)
+
+if __name__ == '__main__':
+    get_string()

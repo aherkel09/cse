a3ab17285710e1d9f624e5d50b24ee47a59ab49f
Avery Herkel <aherkel09@gmail.com>
2019-06-20 10:04:01 -0400

    Update implode.js

--- day05/implode.js
+++ day05/implode.js
@@ -1,1 +1,21 @@
+function implode(string) {
+  var chars = '';
+  
+  for (var s in string) {
+    if (!chars.includes(string[s])) {
+      chars += string[s];
+    }
+  }
+  
+  return chars;
+}
 
+function getStringToImplode() {
+  var string = prompt('Please enter a string:');
+  
+  try {
+    alert(implode(string));
+  } catch(error) {
+    console.log(error);
+  }
+}

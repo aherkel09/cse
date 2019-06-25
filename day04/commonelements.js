8b2379108676968de3b1165ad99b94a1034e1508
Avery Herkel <aherkel09@gmail.com>
2019-06-25 17:44:59 -0400

    alt function, error handling

--- day04/commonelements.js
+++ day04/commonelements.js
@@ -7,5 +7,26 @@ function common(one, two) {
 			}
 		}
 	}
-	console.log(commonArr);
+	return (commonArr);
+}
+
+function getCommonElements(arr1, arr2) {
+	var commonElements = [];
+	for (var a in arr1) { // uses one loop
+		if (arr2.includes(arr1[a])) { // uses built-in includes() function
+			commonElements.push(arr1[a]);
+		}
+	}
+	return commonElements;
+}
+
+function getTwoArrays() {
+	var firstArray = prompt('Enter an array of objects separated by a comma:').split(', ');
+	var secondArray = prompt('Enter a second array:').split(', ');
+	
+	try {
+		alert('The arrays you entered share: ' + getCommonElements(firstArray, secondArray));
+	} catch(error) {
+		console.log(error);
+	}
 }

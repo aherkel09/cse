ceb996d79f9d034d7c9e69f0b5c4cd3b98704312
Avery Herkel <aherkel09@gmail.com>
2019-06-25 17:57:59 -0400

    main loop, function name

--- day04/password_generator.py
+++ day04/password_generator.py
@@ -1,23 +1,25 @@
 import random
 import string
 
-def password():
+def generate_password():
     lower = list(string.ascii_lowercase)
     upper = list(string.ascii_uppercase)
     symbols = ['_', ',', '-']
     length = random.randint(8, 16)
-    passwordString = ""
-    while len(passwordString) < length:
-        passwordString += getRandom(random.randint(0, 2),
-                                    lower, upper, symbols)
+    password_string = ""
+    while len(password_string) < length:
+        password_string += get_random(random.randint(0, 2), lower, upper, symbols)
 
-    print(passwordString)
+    print(password_string)
 
 
-def getRandom(n, lower, upper, symbols):
+def get_random(n, lower, upper, symbols):
     if n == 0:
         return symbols[random.randint(0, 2)]
     elif n == 1:
         return lower[random.randint(0, len(lower)-1)]
     else:
         return upper[random.randint(0, len(upper)-1)]
+
+if __name__ == '__main__':
+    generate_password()

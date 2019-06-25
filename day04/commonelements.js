eb611079d257401ab4080de6138ba9c454b459b8
narmeend13 <51205536+narmeend13@users.noreply.github.com>
2019-06-25 00:52:35 -0400

    Update commonelements.js

--- day04/commonelements.js
+++ day04/commonelements.js
@@ -1,2 +1,11 @@
-function common(one, two){
-  
+function common(one, two) {
+	var commonArr = [];
+	for (var i = 0; i < one.length; i = i + 1) {
+		for (var j = 0; j < two.length; j = j + 1) {
+			if (one[i] == two[j]) {
+				commonArr.push(one[i]);
+			}
+		}
+	}
+	console.log(commonArr);
+}

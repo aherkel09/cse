a1989a88e9f58fe5b5c4befd0333fb314218622f
Avery Herkel <aherkel09@gmail.com>
2019-06-25 11:52:09 -0400

    Update day07_functions.py

--- day07_functions.py
+++ day07_functions.py
@@ -1,1 +1,8 @@
+def set_functions():
+    from day07 import binary_search
+    
+    functions = {
+        1: ('Perform a binary search on a set of 1000 random integers', binary_search.get_number),
+    }
 
+    return functions

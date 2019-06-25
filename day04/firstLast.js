2ba62f9b76ea95610e08b8fb70579eb8eb0989dd
Avery Herkel <aherkel09@gmail.com>
2019-06-25 17:52:30 -0400

    alt function, error handling

---
+++ day04/firstLast.js
@@ -0,0 +1,21 @@
+function first_last(arr) {
+	var newArr = [];
+	newArr.push(arr[0]);
+	newArr.push(arr[arr.length - 1]);
+	console.log(newArr);
+}
+
+function firstLast(arr) {
+	return [arr[0], arr[arr.length - 1]];
+}
+
+function getUserArray() {
+	var array = prompt('Please enter an array of items separated by a comma:').split(', ');
+	
+	try {
+		var firstAndLast = firstLast(array);
+		alert('The first item of your array is ' + firstAndLast[0] + '. The last is ' + firstAndLast[1] + '.');
+	} catch(error) {
+		console.log(error);
+	}
+}
--- day04/first_last.js
+++
@@ -1,6 +0,0 @@
-function first_last(arr) {
-	var newArr = [];
-	newArr.push(arr[0]);
-	newArr.push(arr[arr.length - 1]);
-	console.log(newArr);
-}

ed6a3a845654917e367394fd7432896b70c6b911
narmeend13 <51205536+narmeend13@users.noreply.github.com>
2019-06-25 00:43:16 -0400

    added password

--- day04/password_generator.py
+++ day04/password_generator.py
@@ -1,1 +1,23 @@
 import random
+import string
+
+def password():
+    lower = list(string.ascii_lowercase)
+    upper = list(string.ascii_uppercase)
+    symbols = ['_', ',', '-']
+    length = random.randint(8, 16)
+    passwordString = ""
+    while len(passwordString) < length:
+        passwordString += getRandom(random.randint(0, 2),
+                                    lower, upper, symbols)
+
+    print(passwordString)
+
+
+def getRandom(n, lower, upper, symbols):
+    if n == 0:
+        return symbols[random.randint(0, 2)]
+    elif n == 1:
+        return lower[random.randint(0, len(lower)-1)]
+    else:
+        return upper[random.randint(0, len(upper)-1)]

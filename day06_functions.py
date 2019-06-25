28de109ad8584829ddf4e44fceff2a29796561aa
Avery Herkel <aherkel09@gmail.com>
2019-06-25 13:46:31 -0400

    Update day06_functions.py

--- day06_functions.py
+++ day06_functions.py
@@ -1,1 +1,9 @@
-
+def set_functions():
+    from day06 import read_characters, kwic_index
+    
+    functions = {
+        1: ('See the distribution of letters in a text file', read_characters.choose_file),
+        2: ('View & search the KWIC index of a text file', kwic_index.get_word),
+    }
+    
+    return functions

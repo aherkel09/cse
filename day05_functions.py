33e33d03f58bb6279413deb954410d50c1f29567
Avery Herkel <aherkel09@gmail.com>
2019-06-25 10:37:12 -0400

    alter description

--- day05_functions.py
+++ day05_functions.py
@@ -8,7 +8,7 @@ def set_functions():
         2: ('Enter a dictionary & a key to see how frequently the key occurs', key_frequency.get_data),
         3: ('Enter a dictionary & a value to see which keys are paired with that value', get_keys.get_data),
         4: ('Invert the keys & values of a dictionary', invert_dict.get_data),
-        6: ('See the individual characters of a string with no repeats', implode.get_string)
+        6: ('See the alphabetized characters of a string with no repeats', implode.get_string)
     }
 
     return functions
